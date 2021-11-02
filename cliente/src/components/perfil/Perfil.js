a9cac5cc58d8f1c28d69c1a26e84a7cdc58d7ab8
Max Reveco <max.revecog@hotmail.com>
2021-11-02 20:38:00 -0300

    Se agrega boton like

--- cliente/src/components/perfil/Perfil.js
+++ cliente/src/components/perfil/Perfil.js
@@ -10,7 +10,7 @@ const Perfil = () => {
 
     const context = useContext(UserContext);
     const [post, setPost] = useState([]);
-    const [refresh, setRefresh]=useState(0);
+    const [refresh, setRefresh] = useState(0);
     const [newPost, setNewPost] = useState({
         id_user: "",
         aliasUser: "",
@@ -37,26 +37,46 @@ const Perfil = () => {
         console.log(newPost)
     }
 
+    const meGusta = (e, p) => {
+        e.preventDefault()
+        axios.put(`/api/post/${p._id}`, { userLikes: [...p.userLikes, context.user] })  //Le damos like al post
+            .then(resp => {
+                Swal.fire('Gracias por tu apoyo!', '', 'success')
+                    .then(resp => setRefresh(refresh + 1))
+                    .catch(err => console.log(err))
+            })
+            .catch(err => {
+                Swal.fire('Error al darle me gusta al elemento', '', 'error')
+            })
+    }
+
     const guardarPost = () => {
         const post = {
             id_user: context.user.id,
             aliasUser: context.user.alias,
             content: newPost.content,
             countLikes: 0,
-            userLikes: [context.user]
+            userLikes: []
         }
 
         axios.post('/api/new', post)
             .then(resp => {
                 Swal.fire('Tu idea se registro correctamente', '', 'success')
+                    .then(resp => setRefresh(refresh + 1))
             })
     }
+
+    const logout = () => {
+        localStorage.clear();
+        window.location.href = '/';
+    }
+
 
     return (
         <div className={styles.wrapper}>
             <div className={styles.header}>
                 <h2>Bienvenido: {context.user.alias}</h2>
-                <Button color="danger">Logout</Button>
+                <Button color="danger" onClick={logout}>Logout</Button>
             </div>
             <div className={styles.main}>
                 <Form>
@@ -77,28 +97,28 @@ const Perfil = () => {
                 </Form>
             </div>
             <div className={styles.listaPost}>
-                <Col xs={{size: 6, offset:2}}>
-                <table style={{border:"none"}}>               
-                        { post.map((p, i) => 
-                        <>
-                            <tr key={i}>
-                                <td>{p.aliasUser} ha posteado: </td>
-                                <td className={styles.postContent}>{p.content}</td>                                    
-                            </tr> 
-                            <tr>                                
-                                <td></td>
-                                <td>
-                                    <ul>
-                                        <li><a href="">Me Gusta</a></li>
-                                        <li> Le ha gustado a <a href="">{p.countLikes}</a> personas</li>
-                                        <li>Eliminar</li>
-                                    </ul>
-                                    </td> 
-                            </tr> 
-                        </>                              
-                        )}          
-                </table>   
-                </Col>   
+                <Col xs={{ size: 6, offset: 2 }}>
+                    {post.map((p, i) =>
+                        <table key={i} style={{ border: "none" }}>
+                            <tbody >
+                                <tr>
+                                    <td>{p.aliasUser} ha posteado: </td>
+                                    <td className={styles.postContent}>{p.content}</td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td>
+                                        <ul>
+                                            <li><a href="" onClick={(e) => meGusta(e, p)} >Me Gusta</a></li>
+                                            <li> Le ha gustado a <a href="">{p.countLikes}</a> personas</li>
+                                            <li>Eliminar</li>
+                                        </ul>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    )}
+                </Col>
             </div>
         </div >
     )
